148c07096ce253e40156c39407e4e2266c1d443c
Thanh Nguyen <vnscriptkid@gmail.com>
2024-01-13 01:54:40 +0800

    feat: guest can not create event

--- src/app/events/new/page.tsx
+++ src/app/events/new/page.tsx
@@ -1,7 +1,15 @@
+import { auth } from "@/auth";
 import { EventFormCreate } from "@/components/forms/event-form-create";
+import { redirect } from "next/navigation";
 
 type PageProps = {};
 
-export default function Page(props: PageProps) {
+export default async function Page(props: PageProps) {
+  const session = await auth();
+
+  if (!session || !session.user) {
+    return redirect("/");
+  }
+
   return <EventFormCreate />;
 }
